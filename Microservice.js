e2068832c807296488ed0003987ad95e61fd1e35
Nicolas Herment <nherment@gmail.com>
2014-10-12 09:05:30 +0100

    initial framework

---
+++ Microservice.js
@@ -0,0 +1,88 @@
+
+var patrun = require('patrun')
+var _ = require('lodash')
+
+function Microservice() {
+
+  var stacks = patrun()
+
+  this.add = function(pattern, action) {
+    var existing = stacks.find(pattern);
+    if(existing) {
+      if(_.isEqual(pattern, existing.pattern)) {
+        existing.actions.push(action)
+      }
+    } else {
+      var stackEntry = {
+        pattern: pattern,
+        actions: [action]
+      }
+      stacks.add(pattern, stackEntry)
+    }
+  }
+
+  this.call = function(args, callback) {
+    var callStack = generateCallStack(stacks, args, callback)
+    callStack.run()
+  }
+
+  this.use = function(path) {
+    require(path)(this)
+  }
+}
+
+
+function generateCallStack(stacks, args, callback) {
+  var actions = listActions(stacks, args);
+
+  var executor = {};
+  var executorsChain = executor;
+  for(var i = 0 ; i < actions.length ; i++) {
+    executor.downstream = {}
+    executor.func = actions[i]
+    executor = executor.downstream
+  }
+
+  executor = executorsChain;
+
+  function executeNextChainItem(args, callback) {
+    var prior = function(args, localcb) {
+      if(typeof localcb !== 'function') {
+        throw new Error('callback should be a function')
+      }
+      executor = executor.downstream;
+      executeNextChainItem(args, localcb)
+    }
+    if(executor.func) {
+      try {
+        executor.func.call({prior: prior}, args, callback)
+      } catch(err) {
+        callback(err, undefined)
+      }
+    }
+  }
+
+  return {
+    run: function() {
+      executeNextChainItem(args, callback)
+    }
+  }
+
+}
+
+function listActions(stacks, args) {
+
+  var callStack = [];
+  var argsSpecificStack = stacks.find(args)
+  for(var i = 0 ; i < argsSpecificStack.actions.length ; i++) {
+    var action = argsSpecificStack.actions[i];
+    if(_.isFunction(action)) {
+      callStack.push(action)
+    } else {
+      callStack = callStack.concat(listActions(action, args));
+    }
+  }
+  return callStack;
+}
+
+module.exports = Microservice
---
+++ test/Microservice.test.js
@@ -0,0 +1,117 @@
+
+
+var assert = require('assert')
+var Microservice = require('../Microservice.js')
+var uuid = require('uuid')
+
+describe('Microservice', function() {
+
+  it('one single microservice', function(done) {
+    var microservice = new Microservice()
+
+    var microserviceCalled = false
+    var uniqueResult;
+    microservice.add({foo: 'bar'}, function(args, callback) {
+      assert.ok(args)
+      assert.equal(args.foo, 'bar')
+      microserviceCalled = true
+      uniqueResult = uuid.v4();
+      callback(undefined, uniqueResult)
+    })
+
+    microservice.call({foo: 'bar'}, function(err, result) {
+      assert.ok(!err, err)
+      assert.ok(microserviceCalled)
+      assert.equal(result, uniqueResult)
+      done()
+    })
+
+  })
+
+  it('one single microservice callback(err, undefined)', function(done) {
+    var microservice = new Microservice()
+
+    var microserviceCalled = false
+    var uniqueResult;
+    microservice.add({foo: 'bar'}, function(args, callback) {
+      assert.ok(args)
+      assert.equal(args.foo, 'bar')
+      microserviceCalled = true
+      uniqueResult = uuid.v4();
+      callback(new Error(uniqueResult), undefined)
+    })
+
+    microservice.call({foo: 'bar'}, function(err, result) {
+      assert.ok(err)
+      assert.ok(microserviceCalled)
+      assert.equal(err.message, uniqueResult)
+      done()
+    })
+
+  })
+
+  it('one single microservice throws error', function(done) {
+    var microservice = new Microservice()
+
+    var microserviceCalled = false
+    var uniqueResult;
+    microservice.add({foo: 'bar'}, function(args, callback) {
+      assert.ok(args)
+      assert.equal(args.foo, 'bar')
+      microserviceCalled = true
+      uniqueResult = uuid.v4();
+      throw new Error(uniqueResult)
+    })
+
+    microservice.call({foo: 'bar'}, function(err, result) {
+      assert.ok(err)
+      assert.ok(microserviceCalled)
+      assert.equal(err.message, uniqueResult)
+      done()
+    })
+
+  })
+
+  it('three level single microservice', function(done) {
+    var microservice = new Microservice()
+
+    var nestedMicroservice1Called = false
+    var nestedMicroservice2Called = false
+
+    var result = {}
+    microservice.add({foo: 'bar'}, function(args, callback) {
+      assert.ok(args)
+      assert.equal(args.foo, 'bar')
+      result = {
+          '1': 'ms1'
+      }
+      this.prior(args, callback)
+    })
+    microservice.add({foo: 'bar'}, function(args, callback) {
+      assert.ok(args)
+      assert.equal(args.foo, 'bar')
+      result['2'] = 'ms2';
+      nestedMicroservice1Called = true
+      this.prior(args, callback)
+    })
+    microservice.add({foo: 'bar'}, function(args, callback) {
+      assert.ok(args)
+      assert.equal(args.foo, 'bar')
+      nestedMicroservice2Called = true
+      result['3'] = 'ms3';
+      callback(undefined, result)
+    })
+
+    microservice.call({foo: 'bar'}, function(err, result) {
+      assert.ok(!err, err)
+      assert.ok(nestedMicroservice1Called)
+      assert.ok(nestedMicroservice1Called)
+      assert.equal(result['1'], 'ms1')
+      assert.equal(result['2'], 'ms2')
+      assert.equal(result['3'], 'ms3')
+      done()
+    })
+
+  })
+
+})
